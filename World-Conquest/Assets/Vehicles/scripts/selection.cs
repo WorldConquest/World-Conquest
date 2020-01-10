4d7947e749bc5f562cc0b773095cc1217d128f5a
enzo <enzo.delys@gmail.com>
2020-01-10 17:12:34 +0100

    Sélection des unités

---
+++ World-Conquest/Assets/Vehicles/scripts/selection.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class selection : MonoBehaviour
+{
+    public float speedRotation = 0.6f;
+    GameObject Spotlight;
+
+    void Start()
+    {
+        Spotlight = GameObject.Find ("Spotlight").gameObject;
+    }
+
+    void OnMouseEnter()
+    {
+        Spotlight.transform.position = new Vector3(transform.position.x, Spotlight.transform.position.y, Spotlight.transform.position.z);
+    }
+
+    private void OnMouseOver()
+    {
+        transform.Rotate(0f, speedRotation, 0f);
+    }
+}
