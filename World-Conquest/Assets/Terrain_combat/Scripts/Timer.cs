336f29c2408cefeb7e187ae82e72e716ac0b20d5
Ludovic <ludovic.flament.99@gmail.com>
2020-01-17 13:54:28 +0100

    Timer
    timer added

--- World-Conquest/Assets/Terrain_combat/Scripts/Timer.cs
+++ World-Conquest/Assets/Terrain_combat/Scripts/Timer.cs
@@ -1,35 +1,39 @@
-﻿using System.Collections;
+﻿using System.Threading;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class Timer : MonoBehaviour
 {
     //Define variables
-    public float myCoolTimer = 10;
+    public float timerSec = 120;
     private Text timerText;
 
     void Start()
     {
-        //Link the varaible and the component
+        //Link the variable and the component
         timerText = GetComponent<Text>();    
+
     }
 
     void Update()
     {
-        if(myCoolTimer > 0)
+
+        if(timerSec > 0)
         {
+            //print(timer);
             //Update the value of the component every seconds 
-            myCoolTimer -= Time.deltaTime;
-            timerText.text = myCoolTimer.ToString("f0");
-            print(myCoolTimer);
+            timerSec -= Time.deltaTime*5;
+            timerText.text = timerSec.ToString("f0"); 
+  
         }
-        else if (myCoolTimer < 0 && myCoolTimer > -1)
+        else if (timerSec < 0 && timerSec > -1)
         {
-            timerText.text = myCoolTimer.ToString("Ended !!");
-            print("Ended !!");
+            timerText.text = timerSec.ToString("Ended !!");
             //Make the update function free
-            myCoolTimer = -999;
+            timerSec = -999;
         }
     }
 }
