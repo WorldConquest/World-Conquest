496025398f5a0f35d4621fd6637a98729ffdfa0f
Christian Hecquet <ch.hecquet@live.fr>
2020-01-31 17:39:22 +0100

    Health bar
    Start a new health bar

---
+++ World-Conquest/Assets/Terrain_combat/Health bar/Script/BarScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BarScript : MonoBehaviour
+{
+    [SerializeField] //Allow to see a private variable in unity
+    private float fillAmount;
+
+    [SerializeField]
+    private Image content; // We using namespace Image 
+
+    public float MaxValue { get; set; }
+    public float Value
+    {
+        set
+        {
+            fillAmount = Map(value, 0, MaxValue, 0, 1);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleBar();
+    }
+
+    private void HandleBar()
+    {
+        if (fillAmount != content.fillAmount)
+        {
+            content.fillAmount = fillAmount; //We update a fillAmout if the fillAmout is different at content.fillAmount (the current fillAmount)
+        }
+    }
+
+    private float Map(float value, float inMin, float inMax, float outMin, float outMax)
+    {
+        return (value - inMin) * (outMax - outMin) / (inMax - inMin) + outMin; // To return a value between 0 and 1
+            // ( 80   -   0  ) * (   1   -   0   ) / ( 100% -  0 % ) +   0
+            //        80       *         1         /        100      +   0
+            //  = 0.8 We have 80% of life
+    }
+}
---
+++ World-Conquest/Assets/Terrain_combat/Health bar/Script/Stat.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class Stat
+{
+    [SerializeField]
+    private BarScript bar;
+
+    [SerializeField]
+    private float maxVal;
+
+    [SerializeField]
+    private float currentVal;
+
+    public float CurrentVal 
+    { 
+        get => currentVal; 
+
+        set 
+        { 
+            bar.Value = CurrentVal; currentVal = value; 
+        } 
+    }
+
+    public float MaxVal 
+    { 
+        get => maxVal; 
+
+        set 
+        {
+            this.maxVal = value;
+            bar.MaxValue = maxVal;
+        } 
+    }
+}
