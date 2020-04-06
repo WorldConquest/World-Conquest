36c92fdffaa35747279eb8399340ea63be18b203
Ludovic <ludovic.flament.99@gmail.com>
2020-04-06 20:46:54 +0200

    Latest corrections

--- World-Conquest/Assets/Terrain_combat/Scripts/Popup.cs
+++ World-Conquest/Assets/Terrain_combat/Scripts/Popup.cs
@@ -1,18 +1,51 @@
-﻿using System.Collections;
+﻿using System.Threading;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Popup : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Text message;
+    public GameObject tutorial;
+    public string[] popUps = 
+    {
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7"
+    };
+    private int i=0;
+
     void Start()
     {
-        
-    }
+        //Link the variable and the component
+        message = GetComponent<Text>();   
 
-    // Update is called once per frame
+    }
     void Update()
     {
-        
+        if(Input.GetKeyDown("space") && (i == popUps.Length))
+        {
+            tutorial.SetActive(false);
+        }
+        else if( Input.GetKeyDown("space"))
+        {
+            message.text = popUps[i];
+            i++;
+        }
+
+        //Don't uncomment update, when enter in the else function create a new thread fill all RAM
+        //Find another solution...
+        /*else
+        {
+            Thread.Sleep(2000);
+            message.text = popUps[i];
+            i++;
+        }*/
     }
+
 }
